014c37d6f7d0a907f72d205a372e8e6e6e416ddd
JorgeDGutierrez <83549945+JorgeDGutierrez@users.noreply.github.com>
2021-07-03 02:19:22 -0500

    Add files via upload

---
+++ models/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('../database/config');
+
+class Server {
+	constructor () {
+		this.app = express();
+		this.port = process.env.PORT;
+		this.usuariosPath = '/api/usuarios';
+
+		//Conectar a base de datos
+		this.conectarDB();
+		//Middlewares
+		this.middlewares();
+		//rutas de mi aplicacion
+
+		this.routes();
+	}
+
+	async conectarDB () {
+		await dbConnection();
+	}
+
+	middlewares () {
+		//CORS
+		this.app.use(cors());
+
+		//Parseo y lectura del body
+		this.app.use(express.json());
+
+		//directorio publico
+		this.app.use(express.static('public'));
+	}
+
+	routes () {
+		this.app.use(this.usuariosPath, require('../routes/usuarios.'));
+	}
+
+	Listen () {
+		this.app.listen(this.port, () => {
+			console.log('Servidor corriendo en puerto', this.port);
+		});
+	}
+}
+
+module.exports = Server;
